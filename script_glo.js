0dfcfd6a666d0926976e02a9b2bc32cdb0c6600b
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-26 13:50:21 +0200

    done

--- script_glo.js
+++ script_glo.js
@@ -1,57 +1,57 @@
-"use strict";
-// AJAX  асинхронный JS  
-// // fetch('./db.json')
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//         method:'POST',
-//         body: JSON.stringify({
-//             title: 'foo',
-//             body: 'bar',
-//             userId: 1,
-//           }),
-//           headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//           },
-//         })
+const getData = () => {
+  // Получение данных из файла db.json
+  return fetch("db.json")
+    .then((response) => {
+      if (!response.ok) {
+        // Вывод ошибки, если запрос не удался
+        console.error("Не удалось загрузить данные");
+        return Promise.reject("Не удалось загрузить данные");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      // Вывод ошибки, если произошла ошибка при получении данных
+      console.error("Ошибка:", error);
+      return Promise.reject(error);
+    });
+};
 
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data) // здесь идеальное место для отправки данных в какуюлибо функцию render
-//     })
-//     .catch(error =>{
-//         console.log(error);
-//     })
+const sendData = (data) => {
+  // Отправка данных на сервер
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        // Вывод ошибки, если отправка данных не удалась
+        console.error("Не удалось отправить данные");
+        return Promise.reject("Не удалось отправить данные");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      // Вывод ошибки, если произошла ошибка при отправке данных
+      console.error("Ошибка:", error);
+      return Promise.reject(error);
+    });
+};
 
-
-    const form =document.getElementById("form")
-    const username=document.getElementById('username')
-    const password=document.getElementById('password')
-
-   const sendData =({url, data={}, method ='GET'}) => {
-    return fetch(url, {
-        method: 'POST',
-        body: data,
-        headers: {
-          'Content-type': 'multipart/form-data',
-        },
-      }).then(response =>response.json())
-    }
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault()
-        // const user ={
-        //     login:username.value,
-        //     password: password.value
-        // }
-        // вместо user моем использовать класс конструктов
-        const data = new FormData(form)
-
-        sendData({
-            url:"https://jsonplaceholder.typicode.com/posts",
-            data: data,
-            method:'POST'
-        })
-        .then(data =>{
-          console.log(data)
-        })
-     })
-
-        
+// Получение данных и последующая отправка на сервер
+getData()
+  .then((data) => {
+    // Вывод полученных данных
+    console.log("Данные с сервера:", data);
+    return sendData(data);
+  })
+  .then((responseData) => {
+    // Вывод ответа от сервера
+    console.log("Ответ от сервера:", responseData);
+  })
+  .catch((error) => {
+    // Вывод ошибки, если что-то пошло не так
+    console.error("Ошибка:", error);
+  });
